ecab1ef8b89889be25791ae1f6458c0d4d19de26
javierCueto <javelie@outlook.com>
2020-09-21 12:11:59 -0600

    Initial Commit

---
+++ Fructus-UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus-UI
+//
+//  Created by José Javier Cueto Mejía on 21/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus-UI/Fructus_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fructus_UIApp.swift
+//  Fructus-UI
+//
+//  Created by José Javier Cueto Mejía on 21/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Fructus_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
